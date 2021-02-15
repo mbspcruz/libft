e85f9e70009dee5aff5066977d380545a883c5be
mda.cruz <mar.b.cruz@hotmail.com>
2021-02-15 16:31:19 +0000

    First

---
+++ libft.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mda-cruz <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/09 19:25:41 by mda-cruz          #+#    #+#             */
+/*   Updated: 2021/02/14 17:52:16 by mda-cruz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+#define LIBFT_H
+
+#include <stdio.h>
+
+size_t	ft_strlen(char *str);
+int ft_isalpha(int argument);
+int	ft_isdigit(int arg);
+int ft_isalnum(int argument);
+int ft_isascii(int arg);
+
+#endif
