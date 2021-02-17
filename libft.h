89508b7e6c14b3dd998309bbba14c996048048f5
mda.cruz <mar.b.cruz@hotmail.com>
2021-02-17 18:44:52 +0000

    norm

--- libft.h
+++ libft.h
@@ -6,23 +6,23 @@
 /*   By: mda-cruz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/09 19:25:41 by mda-cruz          #+#    #+#             */
-/*   Updated: 2021/02/17 18:37:42 by mda-cruz         ###   ########.fr       */
+/*   Updated: 2021/02/17 18:44:24 by mda-cruz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
-#define LIBFT_H
+# define LIBFT_H
 
-#include <stdio.h>
+# include <stdio.h>
 
-int	ft_atoi(const char *str);
-int	ft_isalnum(int c);
-int	ft_isalpha(int c);
-int	ft_isascii(int c);
-int	ft_isdigit(int c);
-int	ft_isprint(int c);
+int		ft_atoi(const char *str);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
 size_t	ft_strlen(char *str);
-int	ft_tolower(int c);
-int	ft_toupper(int c);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
 
 #endif
