56b15c0c63fadb3da2c24bc60e2dc613b1321de2
mda.cruz <mar.b.cruz@hotmail.com>
2021-02-17 18:37:51 +0000

    alpha

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mda-cruz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/09 19:25:41 by mda-cruz          #+#    #+#             */
-/*   Updated: 2021/02/17 18:36:26 by mda-cruz         ###   ########.fr       */
+/*   Updated: 2021/02/17 18:37:42 by mda-cruz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,10 @@
 
 #include <stdio.h>
 
-int ft_atoi(const char *str);
-int ft_isalnum(int c);
-int ft_isalpha(int c);
-int ft_isascii(int c);
+int	ft_atoi(const char *str);
+int	ft_isalnum(int c);
+int	ft_isalpha(int c);
+int	ft_isascii(int c);
 int	ft_isdigit(int c);
 int	ft_isprint(int c);
 size_t	ft_strlen(char *str);
