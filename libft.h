4a90c631bf654a8f8177924a8c4a284738b0911b
mda.cruz <mar.b.cruz@hotmail.com>
2021-02-17 18:36:49 +0000

    alpha

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mda-cruz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/09 19:25:41 by mda-cruz          #+#    #+#             */
-/*   Updated: 2021/02/17 18:28:22 by mda-cruz         ###   ########.fr       */
+/*   Updated: 2021/02/17 18:36:26 by mda-cruz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,14 @@
 
 #include <stdio.h>
 
-size_t	ft_strlen(char *str);
-int ft_isalpha(int c);
-int	ft_isdigit(int c);
-int ft_isalnum(int c);
-int ft_isascii(int c);
-int	ft_isprint(int c);
-int	ft_toupper(int c);
-int	ft_tolower(int c);
 int ft_atoi(const char *str);
+int ft_isalnum(int c);
+int ft_isalpha(int c);
+int ft_isascii(int c);
+int	ft_isdigit(int c);
+int	ft_isprint(int c);
+size_t	ft_strlen(char *str);
+int	ft_tolower(int c);
+int	ft_toupper(int c);
 
 #endif
