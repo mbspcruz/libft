e7874ff85755471bf5eaf004597a481dcb048a03
mda.cruz <mar.b.cruz@hotmail.com>
2021-02-17 21:31:42 +0000

    strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,29 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mda-cruz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/09 19:25:41 by mda-cruz          #+#    #+#             */
-/*   Updated: 2021/02/17 21:29:14 by mda-cruz         ###   ########.fr       */
+/*   Created: 2021/02/17 18:56:08 by mda-cruz          #+#    #+#             */
+/*   Updated: 2021/02/17 21:27:00 by mda-cruz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <stdio.h>
+int	ft_strncmp(const char *str1, const char *str2, size_t n)
+{
+	size_t i;
 
-int		ft_isalnum(int c);
-int		ft_isalpha(int c);
-int		ft_isascii(int c);
-int		ft_isdigit(int c);
-int		ft_isprint(int c);
-int		ft_tolower(int c);
-int		ft_toupper(int c);
-int		ft_atoi(const char *str);
-int		ft_strncmp(const char *str1, const char *str2, size_t n);
-size_t	ft_strlen(char *str);
-
-#endif
+	i = 0;
+	if (n == 0)
+		return (0);
+	while (str1[i] != '\0' && str1[i] == str2[i] && i < n - 1)
+		i++;
+	return (str1[i] - str2[i]);
+}
