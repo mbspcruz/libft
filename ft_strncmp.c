3a2c162e170fcbb4460b8c20ddb5240b4c3da312
mda.cruz <mar.b.cruz@hotmail.com>
2021-03-01 20:28:32 +0000

    strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mda-cruz <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: mda-cruz <mda-cruz@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/17 18:56:08 by mda-cruz          #+#    #+#             */
-/*   Updated: 2021/02/17 21:27:00 by mda-cruz         ###   ########.fr       */
+/*   Updated: 2021/03/01 20:28:02 by mda-cruz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,15 @@
 int	ft_strncmp(const char *str1, const char *str2, size_t n)
 {
 	size_t i;
+	unsigned char *s1;
+	unsigned char *s2; 
 
+	s1 = (unsigned char*)str1;
+	s2 = (unsigned char*)str2;
 	i = 0;
 	if (n == 0)
 		return (0);
-	while (str1[i] != '\0' && str1[i] == str2[i] && i < n - 1)
+	while (s1[i] != '\0' && s1[i] == s2[i] && i < n - 1)
 		i++;
-	return (str1[i] - str2[i]);
+	return (s1[i] - s2[i]);
 }
