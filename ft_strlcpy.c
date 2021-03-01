2692a8c0b87a59662516aa944d34fea88d0286e4
mda.cruz <mar.b.cruz@hotmail.com>
2021-03-01 23:27:15 +0000

    strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mda-cruz <mda-cruz@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mda-cruz <mda-cruz@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/04 00:23:46 by mda-cruz          #+#    #+#             */
-/*   Updated: 2021/01/04 00:24:01 by mda-cruz         ###   ########.fr       */
+/*   Updated: 2021/03/01 23:26:36 by mda-cruz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,20 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 	size_t i;
 
 	i = 0;
-	while (src[i] != '\0' && i < size)
+	if (size == 0)
+	{
+		while(src[i])
+			i++;
+		return(i);
+	}
+	while (src[i] != '\0' && i < (size - 1))
 	{
 		dest[i] = src[i];
 		i++;
-		dest[i] = '\0';
 	}
-	return (dest[i]);
+	if (i < size)
+		dest[i] = '\0';
+	while (src[i] != '\0')
+		i++;
+	return (i);
 }
